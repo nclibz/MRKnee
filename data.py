fce3386c67e6686746302b2dcbbd45fac60729c8
nclibz <12936029+nclibz@users.noreply.github.com>
2021-01-05 13:05:38 +0100

    efficientnet

--- data.py
+++ data.py
@@ -3,8 +3,6 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import pytorch_lightning as pl
-import glob
-import pickle
 import numpy as np
 import csv
 torch.manual_seed(17)
@@ -15,32 +13,39 @@ torch.manual_seed(17)
 # lave visualiser for at se hvad dl spytter ud
 # lave en yaml til conda
 # lave transforms
+# Skal lige undersøge normaliseringen??
+# imagenet: normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+# std=[0.229, 0.224, 0.225])
 
+
+ax, cor, bla = [plane for plane in ["axial", "coronal", "bla"]]
 
 # %%
 
+
 class MRDS(Dataset):
-    def __init__(self, stage, diagnosis, plane, transform=None):
+    def __init__(self, stage, diagnosis, transform=None):
         super().__init__()
-        path = f'data/{stage}/{plane}/'
         self.transform = transform
+        self.stage = stage
 
         # label_dict
         with open(f'data/{stage}-{diagnosis}.csv', "r") as f:
-            self.label_dict = {f'{path}{row[0]}.npy': int(row[1])
+            self.label_dict = {row[0]: int(row[1])
                                for row in list(csv.reader(f))}
 
         self.labels = list(self.label_dict.values())
-        self.paths = list(self.label_dict.keys())
+        self.ids = list(self.label_dict.keys())
 
         # calculate  pos_weight
         pos_count = np.sum(self.labels)
         neg_count = len(self.labels) - pos_count
         self.weight = torch.as_tensor(neg_count / pos_count).unsqueeze(0)
 
-    def __getitem__(self, index):
-        # load images
-        series = torch.from_numpy(np.load(self.paths[index])).to(dtype=torch.float32)
+    def prep_series(self, plane, index):
+        id = self.ids[index]
+        path = f'data/{self.stage}/{plane}/{id}.npy'
+        series = torch.from_numpy(np.load(path)).to(dtype=torch.float32)
 
         # transforms
         if self.transform:
@@ -48,34 +53,40 @@ class MRDS(Dataset):
 
         # convert to 3chan
         series = torch.stack((series,)*3, axis=1)
+        return series
+
+    def __getitem__(self, index):
+        # load imgs
+
+        series = [self.prep_series(plane, index)
+                  for plane in ['axial', 'sagittal', 'coronal']]
 
         #  label
         label = torch.as_tensor(int(self.labels[index])).unsqueeze(
             0).to(dtype=torch.float32)
 
         # sample_id
-        sample_path = self.paths[index]
-        return series, label, sample_path, self.weight
+        sample_id = self.ids[index]
+        return series, label, sample_id, self.weight
 
     def __len__(self):
         return len(self.labels)
 
 
 # %%
-
-
 class MRKneeDataModule(pl.LightningDataModule):
 
-    def __init__(self, diagnosis, plane):
+    def __init__(self, diagnosis):
         super().__init__()
         self.diagnosis = diagnosis
-        self.plane = plane
         self.train_transforms = transforms.Compose([
             transforms.CenterCrop(224)
         ])
-        self.train_ds = MRDS('train', self.diagnosis,
-                             self.plane, self.train_transforms)
-        self.val_ds = MRDS('valid', self.diagnosis, self.plane)
+        self.val_transforms = transforms.Compose([
+            transforms.CenterCrop(224)
+        ])
+        self.train_ds = MRDS('train', self.diagnosis,  self.train_transforms)
+        self.val_ds = MRDS('valid', self.diagnosis, self.val_transforms)
 
     # create datasets
 
--- train.py
+++ train.py
@@ -1,24 +1,27 @@
 # %%
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
-
 from model import MRKnee
 from data import MRKneeDataModule
 
+%load_ext autoreload
+%autoreload 2
+
+
 # TODO:
 
-# implement argparser? Behøver vel egentlig kun argparser til predict?? pl har argparser helper
+# lave argparser - gør det nemmere at køre i terminal mens jeg kan rette kode
 # Tune hyperparams
 # error analysis - find top losses - visualise!
 
 # %%
 
-dm = MRKneeDataModule(plane="axial", diagnosis="acl")
-
+dm = MRKneeDataModule(diagnosis="acl")
 model = MRKnee()
 
-# %%
 tb_logger = pl_loggers.TensorBoardLogger('logs/')
 
-trainer = pl.Trainer(gpus=1, max_epochs=1, logger=tb_logger)
+trainer = pl.Trainer(gpus=1, fast_dev_run=True, logger=tb_logger)
 trainer.fit(model, dm)
+
+# %%
