d25c8f5e14932aaf5b7ddde4d5d5a6d7feadfb17
nclibz <12936029+nclibz@users.noreply.github.com>
2021-01-10 12:11:07 +0100

    as

--- data.py
+++ data.py
@@ -86,10 +86,8 @@ class MRKneeDataModule(pl.LightningDataModule):
         self.val_transforms = None
         if transf:
             self.train_transforms = transforms.Compose([
-                transforms.ToPILImage(),
                 transforms.CenterCrop(240),
-                transforms.RandomAffine(25, translate=(0.25, 0.25)),
-                transforms.ToTensor()  # den scaler!!
+                transforms.RandomAffine(25, translate=(0.25, 0.25))
                 # transforms.Normalize(mean=[MEAN], std=[STD]) afprøver bn layer som første input istedet
             ])
             self.val_transforms = transforms.Compose([
