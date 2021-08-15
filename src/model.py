8fa6c2176cee0f2718fc075b6e0854809d06a32d
nsandau <12936029+nclibz@users.noreply.github.com>
2021-08-15 11:59:57 +0200

    gradcam

--- src/model.py
+++ src/model.py
@@ -1,7 +1,6 @@
-
 # %%
 import pytorch_lightning as pl
-from pytorch_lightning.metrics.functional.classification import auroc
+from torchmetrics.functional.classification import auroc
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,24 +11,26 @@ import timm
 
 
 class MRKnee(pl.LightningModule):
-    def __init__(self,
-                 backbone='efficientnet_b0',
-                 pretrained=True,
-                 n_chans=1,
-                 drop_rate=0.0,
-                 final_drop=0.0,
-                 learning_rate=0.0001,
-                 freeze_from=-1,
-                 unfreeze_epoch=0,
-                 planes=['axial', 'sagittal', 'coronal'],
-                 log_auc=True,
-                 log_ind_loss=False,
-                 final_pool='max',
-                 lstm=False,
-                 lstm_layers=1,
-                 lstm_h_size=512,
-                 adam_wd=0.01,
-                 ** kwargs):
+    def __init__(
+        self,
+        backbone="efficientnet_b0",
+        pretrained=True,
+        n_chans=1,
+        drop_rate=0.0,
+        final_drop=0.0,
+        learning_rate=0.0001,
+        freeze_from=-1,
+        unfreeze_epoch=0,
+        planes=["axial", "sagittal", "coronal"],
+        log_auc=True,
+        log_ind_loss=False,
+        final_pool="max",
+        lstm=False,
+        lstm_layers=1,
+        lstm_h_size=512,
+        adam_wd=0.01,
+        **kwargs
+    ):
         super().__init__()
         self.learning_rate = learning_rate
         self.freeze_from = freeze_from
@@ -37,33 +38,43 @@ class MRKnee(pl.LightningModule):
         self.log_auc = log_auc
         self.log_ind_loss = log_ind_loss
         self.n_planes = len(planes)
-        self.backbones = [timm.create_model(backbone, pretrained=pretrained, num_classes=0,
-                                            in_chans=n_chans, drop_rate=drop_rate, ) for i in range(self.n_planes)]
+        self.backbones = [
+            timm.create_model(
+                backbone,
+                pretrained=pretrained,
+                num_classes=0,
+                in_chans=n_chans,
+                drop_rate=drop_rate,
+            )
+            for i in range(self.n_planes)
+        ]
         self.num_features = self.backbones[0].num_features
         self.final_pool = final_pool
         self.final_drop = nn.Dropout(p=final_drop)
         self.adam_wd = adam_wd
 
         # freeze backbones
-        self.backbones = ModuleList([self._freeze(module.as_sequential(), freeze_from)
-                                     for module in self.backbones])
-        self.clf = nn.Linear(self.num_features*self.n_planes, 1)
+        self.backbones = ModuleList(
+            [self._freeze(module.as_sequential(), freeze_from) for module in self.backbones]
+        )
+        self.clf = nn.Linear(self.num_features * self.n_planes, 1)
 
         # lstm
         self.do_lstm = lstm
         self.lstm_layers = lstm_layers
         self.lstm_h_size = lstm_h_size
         if self.do_lstm:
-            self.lstm = nn.LSTM(input_size=self.num_features,
-                                hidden_size=lstm_h_size,
-                                num_layers=lstm_layers,
-                                batch_first=True, bidirectional=False
-                                )
+            self.lstm = nn.LSTM(
+                input_size=self.num_features,
+                hidden_size=lstm_h_size,
+                num_layers=lstm_layers,
+                batch_first=True,
+                bidirectional=False,
+            )
             self.clf = nn.Linear(lstm_h_size, 1)
 
     def forward(self, x):
-        x = [self.run_model(model, series)
-             for model, series in zip(self.backbones, x)]
+        x = [self.run_model(model, series) for model, series in zip(self.backbones, x)]
         x = torch.cat(x, 1)
         x = self.final_drop(x)
         x = self.clf(x)
@@ -76,57 +87,58 @@ class MRKnee(pl.LightningModule):
         # lstm
         if self.do_lstm:
             x = self.final_drop(x)
-            h0 = torch.zeros(self.lstm_layers, x.size(0),
-                             self.lstm_h_size).to(self.device)
-            c0 = torch.zeros(self.lstm_layers, x.size(0),
-                             self.lstm_h_size).to(self.device)
+            h0 = torch.zeros(self.lstm_layers, x.size(0), self.lstm_h_size).to(self.device)
+            c0 = torch.zeros(self.lstm_layers, x.size(0), self.lstm_h_size).to(self.device)
             x, _ = self.lstm(x, (h0, c0))  # (1, num_imgs, lstm_h_size)
 
         # final pooling
 
-        if self.final_pool == 'max':
+        if self.final_pool == "max":
             x = F.adaptive_max_pool2d(x, (1, x.size(-1)))
             x = x.squeeze(0)  # (1, num_features_out)
-        elif self.final_pool == 'avg':
+        elif self.final_pool == "avg":
             x = F.adaptive_avg_pool2d(x, (1, x.size(-1)))
             x = x.squeeze(0)
-        elif self.final_pool == 'last_t_step':
+        elif self.final_pool == "last_t_step":
             x = x[:, -1, :]  # ( 1, num_features_out)
         return x  # (1, num_features_out)
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
-            self.parameters(), lr=self.learning_rate, weight_decay=self.adam_wd)
+            self.parameters(), lr=self.learning_rate, weight_decay=self.adam_wd
+        )
 
         return {
-            'optimizer': optimizer,
-            'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, threshold=1e-4),
-            'monitor': 'val_loss'}
+            "optimizer": optimizer,
+            "lr_scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, patience=5, threshold=1e-4
+            ),
+            "monitor": "val_loss",
+        }
 
     def training_step(self, batch, batchidx):
         imgs, label, sample_id, weight = batch
         logit = self(imgs)
-        loss = F.binary_cross_entropy_with_logits(
-            logit, label, pos_weight=weight)
+        loss = F.binary_cross_entropy_with_logits(logit, label, pos_weight=weight)
 
         # logging
-        self.log('train_loss', loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True, on_step=False)
         return loss
 
     def on_train_epoch_start(self):
         if self.current_epoch == self.unfreeze_epoch:
-            self.backbones = ModuleList([self._unfreeze(module, self.freeze_from)
-                                         for module in self.backbones])
+            self.backbones = ModuleList(
+                [self._unfreeze(module, self.freeze_from) for module in self.backbones]
+            )
 
     def validation_step(self, batch, batchidx):
         imgs, label, sample_id, weight = batch
         logit = self(imgs)
-        loss = F.binary_cross_entropy_with_logits(
-            logit, label, pos_weight=weight)
+        loss = F.binary_cross_entropy_with_logits(logit, label, pos_weight=weight)
 
         # logging
 
-        self.log('val_loss', loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True, on_step=False)
         if self.log_auc:
             self.preds.append(torch.sigmoid(logit).squeeze(0))
             self.lbl.append(label.squeeze(0))
@@ -139,8 +151,12 @@ class MRKnee(pl.LightningModule):
 
     def on_validation_epoch_end(self):
         if self.log_auc:
-            self.log('val_auc', auroc(torch.cat(self.preds), torch.cat(self.lbl), pos_label=1),
-                     prog_bar=True, on_epoch=True)
+            self.log(
+                "val_auc",
+                auroc(torch.cat(self.preds), torch.cat(self.lbl), pos_label=1),
+                prog_bar=True,
+                on_epoch=True,
+            )
 
     def _unfreeze(self, module, idx):
         for param in module[idx:].parameters():
--- src/submit_utils.py
+++ src/submit_utils.py
@@ -1,7 +1,10 @@
+# %%
 import numpy as np
 import albumentations as A
 import torch
 from model import MRKnee
+
+# %%
 
 
 def do_aug(imgs, transf):
@@ -9,11 +12,11 @@ def do_aug(imgs, transf):
     target_dict = {}
     for i in range(imgs.shape[0]):
         if i == 0:
-            img_dict['image'] = imgs[i, :, :]
+            img_dict["image"] = imgs[i, :, :]
         else:
-            img_name = 'image'+f'{i}'
+            img_name = "image" + f"{i}"
             img_dict[img_name] = imgs[i, :, :]
-            target_dict[img_name] = 'image'
+            target_dict[img_name] = "image"
     transf = A.Compose(transf)
     transf.add_targets(target_dict)
     out = transf(**img_dict)
@@ -21,17 +24,17 @@ def do_aug(imgs, transf):
     return out  # returns list of np arrays
 
 
-class Model():
-    def __init__(self, ckpt, plane, backbone, device='cuda'):
+class Model:
+    def __init__(self, ckpt, plane, backbone, device="cuda"):
         self.plane = plane
         self.device = torch.device(device)
         model = MRKnee.load_from_checkpoint(ckpt, planes=[plane], backbone=backbone)
         model.freeze()
         self.model = model.to(device=self.device)
         self.preds = []
-        if 'b0' in backbone:
+        if "b0" in backbone:
             self.img_sz = 224
-        elif 'b1' in backbone:
+        elif "b1" in backbone:
             self.img_sz = 240
 
     def prep_img(self, path, plane):
@@ -39,13 +42,13 @@ class Model():
         imgs = do_aug(imgs, transf=[A.CenterCrop(self.img_sz, self.img_sz)])
         imgs = torch.as_tensor(imgs, dtype=torch.float32)
         imgs = (imgs - imgs.min()) / (imgs.max() - imgs.min()) * 255
-        if plane == 'axial':
+        if plane == "axial":
             MEAN, SD = 66.4869, 60.8146
-        elif plane == 'sagittal':
+        elif plane == "sagittal":
             MEAN, SD = 60.0440, 48.3106
-        elif plane == 'coronal':
+        elif plane == "coronal":
             MEAN, SD = 61.9277, 64.2818
-        imgs = (imgs - MEAN)/SD
+        imgs = (imgs - MEAN) / SD
         imgs = imgs.unsqueeze(1)  # create channel dim
         imgs = imgs.unsqueeze(0)  # create batch dim
         imgs = imgs.to(device=self.device)
@@ -62,21 +65,28 @@ def gather_preds(df, plane):
     paths = df[df[0].str.contains(plane)][0].tolist()
 
     # cnns
-    if plane == 'axial':
-        ckpts = {'abnormal_axial': 'efficientnet_b1',
-                 'acl_axial': 'efficientnet_b1',
-                 'meniscus_axial': 'efficientnet_b1'}
-    if plane == 'sagittal':
-        ckpts = {'abnormal_sagittal': 'efficientnet_b1',
-                 'acl_sagittal': 'efficientnet_b1',
-                 'meniscus_sagittal': 'efficientnet_b0'}
-    if plane == 'coronal':
-        ckpts = {'abnormal_coronal': 'efficientnet_b1',
-                 'acl_coronal': 'efficientnet_b1',
-                 'meniscus_coronal': 'efficientnet_b0'}
+    if plane == "axial":
+        ckpts = {
+            "abnormal_axial": "efficientnet_b1",
+            "acl_axial": "efficientnet_b1",
+            "meniscus_axial": "efficientnet_b1",
+        }
+    if plane == "sagittal":
+        ckpts = {
+            "abnormal_sagittal": "efficientnet_b1",
+            "acl_sagittal": "efficientnet_b1",
+            "meniscus_sagittal": "efficientnet_b0",
+        }
+    if plane == "coronal":
+        ckpts = {
+            "abnormal_coronal": "efficientnet_b1",
+            "acl_coronal": "efficientnet_b1",
+            "meniscus_coronal": "efficientnet_b0",
+        }
 
-    models = [Model(f'src/models/{ckpt}.ckpt', plane, backbone)
-              for ckpt, backbone in ckpts.items()]
+    models = [
+        Model(f"src/models/{ckpt}.ckpt", plane, backbone) for ckpt, backbone in ckpts.items()
+    ]
 
     for model in models:
         for path in paths:
