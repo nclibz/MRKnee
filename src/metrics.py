9aa15bf22bbbb35b4e444187d90eb964f27753ac
nsandau <12936029+nclibz@users.noreply.github.com>
2022-04-19 10:58:03 +0200

    metric typing fix again

--- src/metrics.py
+++ src/metrics.py
@@ -21,28 +21,6 @@ class Metric(ABC):
 
 
 @dataclass
-class MetricLogger:
-    train_metrics: Dict[str, Metric]
-    val_metrics: Dict[str, Metric]
-
-    def __post_init__(self):
-        self.set_attributes()
-
-    def set_attributes(self):
-        all_metrics = self.train_metrics | self.val_metrics
-        for k, v in all_metrics.items():
-            setattr(self, k, v)
-
-    def log_step(self, stage, preds, targets, loss):
-        metrics = self.train_metrics if stage == "train" else self.val_metrics
-        _ = [m.log_step(preds, targets, loss) for m in metrics.values()]
-
-    def log_epoch(self, stage):
-        metrics = self.train_metrics if stage == "train" else self.val_metrics
-        _ = [m.log_epoch() for m in metrics.values()]
-
-
-@dataclass
 class AUC(Metric):
     step_preds: List[torch.Tensor] = field(default_factory=list)
     step_targets: List[torch.Tensor] = field(default_factory=list)
@@ -70,3 +48,25 @@ class Loss(Metric):
         mean_loss = torch.mean(loss)
         self.epoch_values.append(mean_loss)
         self.step_values = []
+
+
+@dataclass
+class MetricLogger:
+    train_metrics: Dict[str, Metric]
+    val_metrics: Dict[str, Metric]
+
+    def __post_init__(self):
+        self.set_attributes()
+
+    def set_attributes(self):
+        all_metrics = self.train_metrics | self.val_metrics
+        for k, v in all_metrics.items():
+            setattr(self, k, v)
+
+    def log_step(self, stage, preds, targets, loss):
+        metrics = self.train_metrics if stage == "train" else self.val_metrics
+        _ = [m.log_step(preds, targets, loss) for m in metrics.values()]
+
+    def log_epoch(self, stage):
+        metrics = self.train_metrics if stage == "train" else self.val_metrics
+        _ = [m.log_epoch() for m in metrics.values()]
